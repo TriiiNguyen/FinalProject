9237a9369782e80a13135e850579b86fa7e3d6ab
TriiiHaus <nguytri8598@gmail.com>
2021-11-20 13:37:51 -0800

    created EMERGENCCY button for the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllMatchups } from '../utils/api';
-
+import { EmergencyButton } from '../Components/EmergencyButton'
 const Home = () => {
   const [matchupList, setMatchupList] = useState([]);
 
@@ -22,31 +22,35 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        <ul className="square">
-          {matchupList.map((matchup) => {
-            return (
-              <li key={matchup._id}>
-                <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                  {matchup.tech1} vs. {matchup.tech2}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
-        </Link>
-      </div>
+    <div className='Button'> 
+      <button onClick={() => console.log('SEND HELP')} type="button" className='btn btn--danger--solid btn--large'>HELP</button>
     </div>
+
+    // <div className="card bg-white card-rounded w-50">
+    //   <div className="card-header bg-dark text-center">
+    //     <h1>Welcome to Tech Matchup!</h1>
+    //   </div>
+    //   <div className="card-body m-5">
+    //     <h2>Here is a list of matchups you can vote on:</h2>
+    //     <ul className="square">
+    //       {matchupList.map((matchup) => {
+    //         return (
+    //           <li key={matchup._id}>
+    //             <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+    //               {matchup.tech1} vs. {matchup.tech2}
+    //             </Link>
+    //           </li>
+    //         );
+    //       })}
+    //     </ul>
+    //   </div>
+    //   <div className="card-footer text-center m-3">
+    //     <h2>Ready to create a new matchup?</h2>
+    //     <Link to="/matchup">
+    //       <button className="btn btn-lg btn-danger">Create Matchup!</button>
+    //     </Link>
+    //   </div>
+    // </div>
   );
 };
 
