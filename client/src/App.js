2accfd8c727e8513ad8777746280ee8904654393
TriiiHaus <nguytri8598@gmail.com>
2021-11-22 21:30:59 -0800

    deleted NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 // import Matchup from './pages/Matchup';
 // import Vote from './pages/Vote';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   return (
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from 'react';
 //import { Link } from 'react-router-dom';
-// need to import model
-import { getAllMatchups } from '../utils/api';
 import { EmergencyButton } from '../Components/EmergencyButton';
+import { SwitchButton } from '../Components/SwitchPageButton';
+
 //import { getAllMatchups, sendMessage } from '../utils/api';
 
 const Home = () => {
   // need to bring in the model name
-  const [matchupList, setMatchupList] = useState([]);
+  // const [matchupList, setMatchupList] = useState([]);
 
-  useEffect(() => {
-    // need to bring in the model and get all the models
-    const getMatchupList = async () => {
-      try {
-        const res = await getAllMatchups();
-        if (!res.ok) {
-          throw new Error('No list of matchups');
-        }
-        const matchupList = await res.json();
-        setMatchupList(matchupList);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    // get model list
-    getMatchupList();
-  }, []);
+  // useEffect(() => {
+  //   // need to bring in the model and get all the models
+  //   const getMatchupList = async () => {
+  //     try {
+  //       const res = await getAllMatchups();
+  //       if (!res.ok) {
+  //         throw new Error('No list of matchups');
+  //       }
+  //       const matchupList = await res.json();
+  //       setMatchupList(matchupList);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+  //   // get model list
+  //   getMatchupList();
+  // }, []);
 
   //Emergency Button click function 
 
-  const handleClick = (event) => {
+  const handleEmergencyClick = (event) => {
     event.preventDefault();
     console.log('SEND HELP');
     //calling api from the utils api file to send a message to the user 
@@ -40,17 +40,30 @@ const Home = () => {
     // })
 
   }
+  
+  const handleSwitchClick = (event) => {
+    event.preventDefault();
+    console.log('Switch To News Page')
+  }
 
   return (
+  <>
     <div className='Button align' >
       {/* <button onClick={() => console.log('SEND HELP')} type="button" className='btn btn--danger--solid btn--xlarge'>HELP</button> */}
       <EmergencyButton
         buttonSize="btn--xlarge"
         buttonStyle="btn--danger--solid"
-        onClick={handleClick}
+        onClick={handleEmergencyClick}
       />
     </div>
-
+   
+    <div>
+      <SwitchButton
+        buttonSize="btn--medium"
+        buttonStyle="btn--warning--outline"
+        onClick={handleSwitchClick}></SwitchButton>
+    </div>
+  </>
   )
 };
 
