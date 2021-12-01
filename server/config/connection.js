c9b72916500db02df004fe094634939e6e0a16e1
TriiiHaus <nguytri8598@gmail.com>
2021-12-01 00:25:12 -0800

    node version specified

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/alertMe',
+  process.env.MONGODB_URI || 'mongodb://localhost/alertme',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
